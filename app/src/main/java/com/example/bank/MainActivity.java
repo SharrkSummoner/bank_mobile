b122de6c999099433837079cf9fa3b204c88346e
SharrkSummoner <akulka20@mail.ru>
2022-06-16 11:23:55 +0300

    app bank

---
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void kurs(View view) {
+    }
+
+    public void office(View view) {
+    }
+}
